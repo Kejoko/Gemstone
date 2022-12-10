af702687611851ae497b366f3946eb694516f132
Kejoko <keeganjkochis@gmail.com>
2022-12-10 11:03:13 -0700

    Move vendor libraries into a seperate cmake file for cleanup: :

---
+++ cmake/VendorLibraries.cmake
@@ -0,0 +1,36 @@
+#====================================================================
+# Third party vendor libraries
+#
+# @note: This file is intended to be included after the following
+#        variables are defined:
+#          * GLAD_SOURCE_DIR
+#          * GLFW_SOURCE_DIR
+#          * SPDLOG_SOURCE_DIR
+#====================================================================
+set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
+
+# OpenGL
+find_package(OpenGL REQUIRED)
+
+# Glad
+add_library(
+    glad
+    SHARED
+    ${GLAD_SOURCE_DIR}/include/glad/glad.h
+    ${GLAD_SOURCE_DIR}/src/glad.c
+)
+list(APPEND VENDOR_INCLUDE_DIRS ${GLAD_SOURCE_DIR}/include)
+list(APPEND VENDOR_LIBS glad)
+
+# GLFW
+set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+add_subdirectory(${GLFW_SOURCE_DIR})
+list(APPEND VENDOR_INCLUDE_DIRS ${GLFW_SOURCE_DIR}/include)
+list(APPEND VENDOR_LIBS glfw)
+
+# SpdLog
+add_subdirectory(${SPDLOG_SOURCE_DIR})
+list(APPEND VENDOR_INCLUDE_DIRS ${SPDLOG_SOURCE_DIR}/include)
+list(APPEND VENDOR_LIBS spdlog)
