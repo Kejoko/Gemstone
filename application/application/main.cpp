c4874467e5c93c067d5e4ec886fe7d24796edcff
Kejoko <keeganjkochis@gmail.com>
2022-12-18 10:04:49 -0700

    MUCH cleaner cmake practices

--- application/application/main.cpp
+++ application/application/main.cpp
@@ -11,8 +11,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-// #include <stb/stb_image.h>
-
 #include "util/macros.hpp"
 #include "util/platform.hpp"
 #include "util/io/logger.hpp"
